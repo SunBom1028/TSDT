f48df5d530007c0d77f5cdb655e3c98ddbfb3030
SUNBOM <15097915767@163.com>
2024-05-18 16:34:55 +0800

    message 8:Before refactoring the functional_tests(test1)

--- test1.py
+++ test1.py
@@ -30,7 +30,7 @@ class NewVisitorTest(unittest.TestCase):
             )
 
         #输入了Buy flowers
-        inputbox.send_keys('Buy_flowers')  #(2)
+        inputbox.send_keys('Buy flowers')  #(2)
 
         #按了回车，页面更新了
         #待办事项列表中显示了1：Buy flowers
@@ -38,18 +38,25 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)  #(4)
 
         table=self.browser.find_element(By.ID,'id_list_table')
-        rows=table.find_element(By.TAG_NAME,'tr')  #(1)
+        rows=table.find_elements(By.TAG_NAME,'tr')  #(1)
         self.assertIn('1: Buy flowers',[row.text for row in rows])
 
         #页面中又出现了一个文本输入框，可以输入其他待办事项
-        #他输入了Send a gift to Lisi
-        self.fail('Finish the test!')
+        #他输入了Give a gift to Lisi
+        inputbox=self.browser.find_element(By.ID,'id_new_item')
+        inputbox.send_keys('Give a gift to Lisi')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         #页面再次更新，清单上显示两个待办事项
+        table=self.browser.find_element(By.ID,'id_list_table')
+        rows=table.find_elements(By.TAG_NAME,'tr')
+        self.assertIn('1: Buy flowers',[row.text for row in rows])
+        self.assertIn('2: Give a gift to Lisi',[row.text for row in rows])
 
         #张三想知道网站是否记住清单
         #他看到网站为他生成了唯一的URL
-
+        self.fail('Finish the test!')
         #他访问了那个URL，发现待办事项还在
         #满意离开
 
